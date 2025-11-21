4c7ce69cb9e4455631539eb3c1f4d05cf3329902
shubhdeep varshney <shubhdeepvarshney02@gmail.com>
2025-11-21 23:43:05 +0530

    add logger

--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -1,7 +1,10 @@
 package net.engineeringdigest.journalApp.service;
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,12 +13,13 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Component
 public class UserService {
     @Autowired
     private UserRepository UserRepository;
     private static final PasswordEncoder passwordEncoder =new BCryptPasswordEncoder();
+    //private Logger logger= LoggerFactory.getLogger(UserService.class);
 
     public boolean saveNewUser(User user){
         try{
@@ -24,6 +28,7 @@ public class UserService {
             UserRepository.save(user);
             return true;
         }catch(Exception e){
+            log.error("hahaha {} :",user.getUsername(),e);
             return false;
         }
     }
